da2171806d82dd0dac6eab4e35fde9eb2073f6d0
pfolkertsma <pfolkertsma@bin106.bin>
2015-04-07 15:57:18 +0200

    Gene Miners Application with unit tests.

---
+++ src/geneminersapplication/GeneMinersApplication.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package geneminersapplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author pfolkertsma & ngnodde
+ */
+public class GeneMinersApplication {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        GeneMinersApplication gma = new GeneMinersApplication();
+        gma.start();
+    }
+    /**
+     * This function starts the functions of the application.
+     */
+    public void start() {
+    }
+    /**
+     * This function returns true if the user exists in the database and false
+     * if it does not.
+     * @param userName the username of the user
+     * @param userPassword the password of the user
+     * @return boolean
+     */
+    public final boolean loginUser(
+            final String userName,
+            final String userPassword) {
+        HashMap<String, String> database = new HashMap();
+        database.put("Zaphod", "president");
+        database.put("Arthur", "thea");
+        database.put("Ford", "beetlejuice");
+        database.put("Trillian", "madagascar");
+        database.put("Marvin", "iamdepressed");
+        if (database.containsKey(userName)) {
+            return database.get(userName).equals(userPassword);
+        }
+        return false;
+    }
+    /**
+     * This function creates a gene object if the given gene name is found and
+     * returns a message, else it returns an error message.
+     * @param geneName the gene name
+     * @return String
+     */
+    public final String searchGene(final String geneName) {
+        ArrayList<String> genes = new ArrayList<>();
+        genes.add("BRCA1");
+        genes.add("CD28");
+        genes.add("BRCA2");
+        genes.add("APC");
+        genes.add("BDNF");
+        genes.add("CRH");
+        genes.add("RHO");
+        if (genes.contains(geneName)) {
+            Gene gene = new Gene();
+            return "Gene object created.";
+        } else {
+            return "Gene not found.";
+        }
+    }
+    /**
+     * This function creates an Experiment object, if Experiment creation fails
+     * an exception is thrown.
+     * @param dataset a dataset
+     * @return boolean
+     */
+    public final boolean loadExpressionDataset(final String dataset) {
+        try {
+            Experiment exp = new Experiment();
+            if (dataset.equals("failDataset")) {
+                throw new Exception();
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println("Failed to create experiment dataset.");
+            return false;
+        }
+    }
+    /**
+     * This function creates a Plot object.
+     * @param genes an ArrayList with genes.
+     * @return boolean
+     */
+    public final boolean createPlot(final ArrayList genes) {
+        try {
+            Plot plot = new Plot();
+            if (genes == null) {
+                throw new Exception();
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println("Failed to create plot.");
+            return false;
+        }
+    }
+    /**
+     * This function creates a Cluster object.
+     * @param genes an ArrayList with genes.
+     * @return boolean
+     */
+    public final boolean createCluster(final ArrayList genes) {
+        try {
+            Cluster cluster = new Cluster();
+            if (genes == null) {
+                throw new Exception();
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println("Failed to create cluster.");
+            return false;
+        }
+    }
+}
---
+++ test/geneminersapplication/GeneMinersApplicationTest.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package geneminersapplication;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author pfolkertsma & ngnodde
+ */
+public class GeneMinersApplicationTest {
+    /**
+     *
+     */
+    public GeneMinersApplicationTest() {
+    }
+    /**
+     *
+     */
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    /**
+     *
+     */
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    /**
+     *
+     */
+    @Before
+    public void setUp() {
+    }
+    /**
+     *
+     */
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Test of loginUser method, of class GeneMinersApplication.
+     */
+    @Test
+    public final void testLoginUser() {
+        System.out.println("loginUser");
+        GeneMinersApplication instance = new GeneMinersApplication();
+        // Test if the user exists.
+        boolean expResult = true;
+        boolean result = instance.loginUser("Zaphod", "president");
+        assertEquals(expResult, result);
+        // Test if the user does not exists or if the password is not correct.
+        expResult = false;
+        result = instance.loginUser("Henk", "123");
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of searchGene method, of class GeneMinersApplication.
+     */
+    @Test
+    public final void testSearchGene() {
+        System.out.println("searchGene");
+        GeneMinersApplication instance = new GeneMinersApplication();
+        // Test if the gene is found.
+        String expResult = "Gene object created.";
+        String result = instance.searchGene("APC");
+        assertEquals(expResult, result);
+        // Test if the gene is not found.
+    }
+    /**
+     * Test of loadExpressionDataset method, of class GeneMinersApplication.
+     */
+    @Test
+    public final void testLoadExpressionDataset() {
+        System.out.println("loadExpressionDataset");
+        GeneMinersApplication instance = new GeneMinersApplication();
+        // Test if the experiment gets created.
+        boolean expResult = true;
+        boolean result = instance.loadExpressionDataset("dataset");
+        assertEquals(expResult, result);
+        // Test if the experiment gets not created.
+        expResult = false;
+        result = instance.loadExpressionDataset("failDataset");
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of createPlot method, of class GeneMinersApplication.
+     */
+    @Test
+    public final void testCreatePlot() {
+        System.out.println("createPlot");
+        GeneMinersApplication instance = new GeneMinersApplication();
+        ArrayList<String> genes = new ArrayList<>();
+        genes.add("BRCA2");
+        genes.add("APC");
+        genes.add("BDNF");
+        // Test if the plot gets created.
+        boolean expResult = true;
+        boolean result = instance.createPlot(genes);
+        assertEquals(expResult, result);
+        // Test if the plot gets not created.
+        expResult = false;
+        result = instance.createPlot(null);
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of createCluster method, of class GeneMinersApplication.
+     */
+    @Test
+    public final void testCreateCluster() {
+        System.out.println("createCluster");
+        GeneMinersApplication instance = new GeneMinersApplication();
+        ArrayList<String> genes = new ArrayList<>();
+        genes.add("BRCA2");
+        genes.add("APC");
+        genes.add("BDNF");
+        // Test if the cluster gets created.
+        boolean expResult = true;
+        boolean result = instance.createCluster(genes);
+        assertEquals(expResult, result);
+        // Test if the cluster gets not created.
+        expResult = false;
+        result = instance.createCluster(null);
+        assertEquals(expResult, result);
+    }
+}
